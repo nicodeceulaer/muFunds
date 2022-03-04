7e75e103885f9452c78a710dc3bb72d7fb60f5b2
De Ceulaer, Nico <nico.de.ceulaer@gmail.com>
2022-03-04 15:34:18 +0100

    add V8 engine

---
+++ test.sh
@@ -0,0 +1,23 @@
+{
+  "Accept-Ranges": "bytes",
+  "Cache-Control": "max-age=180",
+  "expiresdefault": "access plus 2 hours",
+  "x-cache": "Miss from cloudfront",
+  "Content-Encoding": "gzip",
+  "Content-Type": "text/html;charset=UTF-8",
+  "Content-Language": "en-US",
+  "Vary": "Accept-Encoding",
+  "Age": "69",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Origin,X-Requested-With,Content-Type,Accept",
+  "Connection": "keep-alive",
+  "x-amz-cf-id": "l7LaZz8w9ZZvDYyIDlWTmlS18F91y80q7dTL_cDi5xj6s-kJOn2Mpw==",
+  "Date": "Fri, 04 Mar 2022 13:47:06 GMT",
+  "Server": "Apache",
+  "Via": "1.1 varnish (Varnish/6.0), 1.1 67711c5cba0352ee130f60f6cc103e0a.cloudfront.net (CloudFront)",
+  "Content-Length": "2770",
+  "x-amz-cf-pop": "IAD50-C2",
+  "x-varnish": "58851935 58726533",
+  "Pragma": "Pragma",
+  "x-cnection": "close"
+}
