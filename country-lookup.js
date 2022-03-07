8dd64707e430c0333bb8f4e6b09a18274c3d5dd9
De Ceulaer, Nico <nico.de.ceulaer@gmail.com>
2022-03-07 12:42:55 +0100

    country-lookup.js: refactor country dependant code with a single lookup table
    Combine all the country-related info in a single lookup table, so we can easily add a new country.
    (instead of having to add else blocks everywhere in the code)

---
+++ country-lookup.js
@@ -0,0 +1,96 @@
+function countryLookup( country, option) {
+  const lookup = {
+    "au": {
+      'url': "https://www.morningstar.com.au",
+    },
+    "es": {
+      'url': "http://www.morningstar.es/es",
+    },
+    "de": {
+      'url': "http://www.morningstar.de/de",
+      'aum': 'Fondsvolumen',
+      'nav': ['Schlusskurs', 'NAV'],
+      'date': ['Schlusskurs', 'NAV'],
+      'change': 'Änderung z. Vortag',
+      'currency': ['Schlusskurs', 'NAV'],
+      'expenses': 'Laufende Kosten',
+      'category': 'Morningstar Kategorie'
+    },
+    "ie": {
+      'url': "http://www.morningstarfunds.ie/ie",
+    },
+    "fr": {
+      'url': "http://www.morningstar.fr/fr",
+    },
+    "za": {
+      'url': "http://www.morningstar.co.za/za",
+    },
+    "at": {
+      'url': "http://www.morningstar.at/at",
+    },
+    "be": {
+      'url': "http://www.morningstar.be/be",
+      'aum': 'Grootte Fonds ',
+      'nav': ['slotkoers', 'Koers'],
+      'date': ['slotkoers', 'Koers'],
+      'change': 'Rendement 1 Dag',
+      'currency': ['slotkoers', 'Koers'],
+      'expenses': 'Lopende Kosten Factor',
+      'category': 'Morningstar Categorie'
+    },
+    "dk": {
+      'url': "http://www.morningstar.dk/dk",
+    },
+    "fi": {
+      'url': "http://www.morningstar.fi/fi",
+    },
+    "gb": {
+      'url': "http://www.morningstar.co.uk/uk",
+    },
+    "uk": {
+      'url': "http://www.morningstar.co.uk/uk",
+      'aum': 'Fund Size ',
+      'nav': 'Closing Price',
+      'date': 'Closing Price',
+      'change': 'Day Change',
+      'currency': 'Closing Price',
+      'expenses': 'Ongoing Charge',
+      'category': 'Morningstar Category'
+    },
+    "ch": {
+      'url': "http://www.morningstar.ch/ch",
+    },
+    "is": {
+      'url': "http://www.morningstar.is/is",
+    },
+    "it": {
+      'url': "http://www.morningstar.it/it",
+    },
+    "pt": {
+      'url': "http://www.morningstar.pt/pt",
+    },
+    "no": {
+      'url': "http://www.morningstar.no/no",
+    },
+    "nl": {
+      'url': "http://www.morningstar.nl/nl",
+      'aum': 'Grootte Fonds ',
+      'nav': ['slotkoers', 'Koers'],
+      'date': ['slotkoers', 'Koers'],
+      'change': 'Rendement 1 Dag',
+      'currency': ['slotkoers', 'Koers'],
+      'expenses': 'Lopende Kosten Factor',
+      'category': 'Morningstar Categorie'
+    }
+  };
+
+  const item = lookup[country];
+  if (!item ) {
+    throw new Error("Country " + country + " is not supported");
+  }
+  const value = item[option];
+  if (!value) {
+    throw new Error("The option '" + option + "' is not present for country '" + country + "'");
+  }
+  return value;
+}
