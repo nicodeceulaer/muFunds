fd9b74fa6a10660e5d6443a8a31da76566c1add1
De Ceulaer, Nico <nico.de.ceulaer@gmail.com>
2022-03-07 12:39:53 +0100

    main.js: add test_single() to automate tests further
    Still under construction. Allow testing of individual combinations, so we can end up with a list of tests.

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 /* ------------------- Main function -------------------- */
 
+function test() {
+  // test_single('morningstar-be', 'BE6299843811');
+  // test_single('morningstar-be', 'BE6301090674');
+  // test_single('morningstar-de', 'US46137V5892');
+  // test_single('morningstar-nl', 'LU0255977372');
+  // test_single('morningstar-de', 'LU1171478784');
+  test_single('morningstar-de', 'LU0690375422');
+}
+
+function test_single(source, id) {
+  // const options = ['nav', 'date', 'change', 'currency', 'expenses', 'category', 'source', 'aum'];
+  const options = ['nav'];
+  
+  for (const option of options) {
+    Logger.log('Test(id:' + id + ', option:' + option + ', source: ' + source+ ')');
+    let result = muFunds(option, id, source);
+    Logger.log('result: ' + result + ' (id:' + id + ', option:' + option + ', source:' + source + ')');
+  }
+}
+
 /**
  * Imports mutual fund NAV or other data from Morningstar. See complete documentation at mufunds.com.
  *
@@ -9,19 +29,6 @@
  * @return The asked information from the fund, according to the selected source.
  * @customfunction
  */
-function test() {
-  // const options = ['nav', 'date', 'change', 'currency', 'expenses', 'category', 'source'];
-  const options = ['aum'];
-  const sources = ['morningstar-nl'];
-  for (const source of sources) {
-    for (const option of options) {
-      Logger.log('Test for option:' + option + ', source: ' + source);
-      let result = muFunds(option, "IE00B0MTTV84", source);
-      Logger.log('result: ' + result + ' (option=' + option + ', source=' + source + ')');
-    }
-  }
-}
-
 function muFunds(option, id, source) {
   // First, check if option is valid
   if(!( option == "nav" ||
